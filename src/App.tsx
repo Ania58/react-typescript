77ead64988487c7df9139dda1d6ebdbdb1cdcdca
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-05-31 21:13:42 +0200

    Add CourseGoals component with dummy goals and create empty NewGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header.tsx";
 import goalsImg from './assets/goals.jpg';
+import CourseGoals from "./components/CourseGoals.tsx";
 
 function App() {
   
@@ -9,6 +10,19 @@ function App() {
       <Header image={({src: goalsImg, alt:'A list of goals'})}>
         <h1>Your Course Goals</h1>
       </Header>
+      <CourseGoals 
+      goals={[
+        {
+          id:1, 
+          title: 'Learn TS', 
+          description: 'Learn TS from the ground up'
+        },
+        {
+          id:2, 
+          title: 'Practice TS', 
+          description: 'Practice working with TypeScript'
+        }
+      ]} />
     </main>
   )
 }
